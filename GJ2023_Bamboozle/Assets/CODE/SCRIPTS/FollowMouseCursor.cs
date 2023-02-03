ebf68af1121bffa6ea4eb3ef8b7b09884e78db13
Diego Garcia Figueroa <diegogarciafigueroa17@gmail.com>
2023-02-03 15:58:42 -0600

    Finished with EyeTracking

---
+++ GJ2023_Bamboozle/Assets/CODE/SCRIPTS/FollowMouseCursor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouseCursor : MonoBehaviour
+{
+    Vector3 pos;
+
+    public float offset = 5f;
+
+    void Update()
+
+    {
+
+        pos = Input.mousePosition;
+
+        pos.z = offset;
+
+        transform.position = Camera.main.ScreenToWorldPoint(pos);
+
+    }
+
+
+}
